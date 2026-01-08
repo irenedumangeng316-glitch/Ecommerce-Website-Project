cf56a55885e621ba9ed54ef5fc4d0e0c0ac6fc30
sofia gan <jsmaba0928@gmail.com>
2026-01-08 00:11:40 -0500

    framewrok change

--- admin_header.php
+++ admin_header.php
@@ -21,8 +21,8 @@ if (isset($_POST['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://kit.fontawesome.com/f59eee459e.js"></script>
     <title>Admin Dashboard</title>
 </head>
 <body>
